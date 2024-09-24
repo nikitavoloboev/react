24499a5ab57dfbad4cb582f6dccc09180ff52802
Nikita <github@nikiv.dev>
2024-09-24 12:48:12 +0300

    actions

--- app/actions.ts
+++ app/actions.ts
@@ -3,9 +3,17 @@ import { create } from "ronin"
 
 export const createAirdropWalletToClaim = createServerFn("POST", async () => {})
 
-export const testAction = createServerFn("POST", async () => {
-  const res = await create.airdropWalletToClaim.with({
-    walletAddress: "test",
-  })
-  console.log(res)
-})
+export const testAction = createServerFn(
+  "POST",
+  async (data: { walletAddress: string }) => {
+    const { walletAddress } = data
+    // console.log(walletAddress, "wallet address")
+    // console.log(process.env.RONIN_TOKEN, "ronin token")
+    // const res = await create.airdropWalletToClaim.with({
+    //   walletAddress: "test",
+    // })
+    throw new Error("broke")
+    // console.log(res)
+    // return true
+  },
+)
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router"
+import toast from "react-hot-toast"
+import { testAction } from "~/actions"
 
 function RouteComponent() {
   return (
     <>
       <button
-        onClick={() => {
-          console.log("test")
+        onClick={async () => {
+          try {
+            const res = await testAction({ walletAddress: "test" })
+            console.log(res, "res")
+          } catch (err) {
+            // toast.error("error")
+            console.log(err, "err")
+          }
         }}
         className="bg-blue-500 text-white px-4 py-2 rounded"
       >
