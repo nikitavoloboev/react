5ec75f27f190f32e1c8f3f9030ae35f54cda9cae
Nikita <github@nikiv.dev>
2024-10-19 00:29:09 +0200

    refetch works

--- app/routes/refetch.tsx
+++ app/routes/refetch.tsx
@@ -1,6 +1,6 @@
 import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
-import { fetchAction, refetchAction } from "~/actions"
+import { fetchAction } from "~/actions"
 
 function RouteComponent() {
   const { data } = useSuspenseQuery({
@@ -10,12 +10,12 @@ function RouteComponent() {
       return res
     },
   })
+  const queryClient = useQueryClient()
   return (
     <div>
       <div>{data.time}</div>
       <button
         onClick={() => {
-          const queryClient = useQueryClient()
           queryClient.invalidateQueries({
             queryKey: ["original-fetch"],
             refetchType: "all",
